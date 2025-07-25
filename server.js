7b10e03346dd0c4afee7dd2721c4f75df55ee2a4
Ahmpobs <tankodouglas47@gmail.com>
2025-07-25 22:41:26 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"));
+
+app.use('/auth', require('./routes/auth'));
+app.use('/admin', require('./routes/admin'));
+app.use('/support', require('./routes/support'));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
---
+++ user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  balance: { type: Number, default: 0 },
+  referralCode: String,
+  referredBy: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
